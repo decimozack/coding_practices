52e043954a72d2414eaff02438ff780faeb66dc5
Zack Chua Zhi Jie <zackwco9@gmail.com>
2020-08-26 13:38:41 +0800

    improve int_to_roman using pythonic way

--- leetcode/python/int_to_roman.py
+++ leetcode/python/int_to_roman.py
@@ -3,46 +3,15 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         
-        digit_arr = []
+        value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        roman = ['M', 'CM', 'D', 'CD','C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
         
-        while num > 0:
-            
-            digit_arr.append(num % 10)
-            num = num // 10
-            
-        roman_chars = {
-            0: ['I', 'V', 'IV', 'IX'],
-            1: ['X', 'L', 'XL', 'XC'],
-            2: ['C', 'D', 'CD' ,'CM'],
-            3: ['M']
-        }
+        result = ''
         
-        def convert_roman(curr_digit: int, tenths: int, roman_chars) -> str:
-            
-            curr_list = roman_chars[tenths]
-            curr_roman = ''
-            
-            if curr_digit == 4:
-                return curr_list[2]
-            elif curr_digit == 9:
-                return curr_list[3]
-            elif curr_digit == 0:
-                return ''
-            if curr_digit < 4:
-                for i in range(curr_digit):
-                    curr_roman += curr_list[0]
-            elif curr_digit >= 5:
-                curr_roman = curr_list[1]
-                for i in range(curr_digit - 5):
-                    curr_roman += curr_list[0]
-            
-            return curr_roman
+        for val, rom in zip(value, roman):
         
-        roman_str = ''
-        count = 0
-        for curr_digit in digit_arr:
-            
-            roman_str = convert_roman(curr_digit, count, roman_chars) + roman_str
-            count += 1
+            result += (num // val) * rom
+            num %= val
         
-        return roman_str
+        return result
+                
