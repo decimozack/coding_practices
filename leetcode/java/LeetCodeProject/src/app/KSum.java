ebe5957041b0b88fe28a0b3c6eead074ceadba9e
Zack Chua Zhi Jie <zackwco9@gmail.com>
2020-09-28 12:39:46 +0800

    done with ksum

---
+++ leetcode/java/LeetCodeProject/src/app/KSum.java
@@ -0,0 +1,89 @@
+package app;
+
+import java.util.*;
+
+public class KSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 4);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 3);
+    }
+
+    public List<List<Integer>> kSum(int[] nums, int target, int startIdx, int k) {
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if (nums.length < k || nums[startIdx] * k > target || nums[nums.length - 1] * k < target) {
+            return result;
+        }
+
+        if (k == 2) {
+            return twoSum(nums, target, startIdx);
+        }
+
+        for (int i = startIdx; i < nums.length - k + 1; i++) {
+
+            if (i == startIdx || nums[i] != nums[i - 1]) {
+                for (var set : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                    List<Integer> inResult = new ArrayList<Integer>();
+                    inResult.add(nums[i]);
+                    inResult.addAll(set);
+                    result.add(inResult);
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+    public List<List<Integer>> twoSum(int[] nums, int target, int start) {
+
+        int end = nums.length - 1;
+        int sum = 0;
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        while (start < end) {
+
+            sum = nums[start] + nums[end];
+            if (sum == target) {
+                List<Integer> inResult = new ArrayList<Integer>();
+                inResult.add(nums[start]);
+                inResult.add(nums[end]);
+                result.add(inResult);
+
+                while (start < end) {
+                    if (nums[start] == nums[start + 1]) {
+                        start++;
+                    } else {
+                        break;
+                    }
+                }
+
+                while (start < end) {
+                    if (nums[end] == nums[end - 1]) {
+                        end--;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            if (sum < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ leetcode/java/LeetCodeProject/src/test/TestKSum.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import app.KSum;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.*;
+
+public class TestKSum {
+
+    private KSum kSum = new KSum();
+
+    @Test
+    public void testTwoSum() {
+
+        int[] nums = { -2, -1, 0, 0, 1, 2 };
+        int target = 0;
+
+        List<List<Integer>> result = kSum.twoSum(nums, target, 0);
+        List<List<Integer>> expected = new ArrayList<List<Integer>>();
+
+        List<Integer> inExpected = new ArrayList<Integer>();
+        inExpected.add(-2);
+        inExpected.add(2);
+        expected.add(inExpected);
+
+        inExpected = new ArrayList<Integer>();
+        inExpected.add(-1);
+        inExpected.add(1);
+        expected.add(inExpected);
+
+        inExpected = new ArrayList<Integer>();
+        inExpected.add(0);
+        inExpected.add(0);
+        expected.add(inExpected);
+
+        assertEquals(expected, result);
+    }
+}
